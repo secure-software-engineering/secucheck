030837f2e2ac7c93b7567fe9b35969d1098e68f9
Ranjith K <ranjith.masthikatte@gmail.com>
2020-10-05 16:48:39 +0200

    Updated the target to FIELD

--- de.fraunhofer.iem.secucheck.InternalFluentTQL/src/main/java/de/fraunhofer/iem/secucheck/InternalFluentTQL/dsl/annotations/InFlowThisObject.java
+++ de.fraunhofer.iem.secucheck.InternalFluentTQL/src/main/java/de/fraunhofer/iem/secucheck/InternalFluentTQL/dsl/annotations/InFlowThisObject.java
@@ -8,7 +8,7 @@ import java.lang.annotation.*;
  * @author Ranjith Krishnamurthy
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE_USE)
+@Target(ElementType.FIELD)
 @Documented
 public @interface InFlowThisObject {
 }
