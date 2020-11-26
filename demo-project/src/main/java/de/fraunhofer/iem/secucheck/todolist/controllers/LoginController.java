a788b3146940a6fb26bf97781c91d4184379b286
oshando <oshando@campus.uni-paderborn.de>
2020-11-26 23:07:53 +0100

    Remove unused imports

--- demo-project/src/main/java/de/fraunhofer/iem/secucheck/todolist/controllers/LoginController.java
+++ demo-project/src/main/java/de/fraunhofer/iem/secucheck/todolist/controllers/LoginController.java
@@ -1,8 +1,6 @@
 package de.fraunhofer.iem.secucheck.todolist.controllers;
 
 import java.io.IOException;
-import java.net.http.HttpRequest;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
