1fb94ad1e4a60afc1cf168fcc91e333972d24b76
Ranjith K <ranjith.masthikatte@gmail.com>
2021-04-15 19:26:10 +0200

    Implements hashCode, equals and toString

--- de.fraunhofer.iem.secucheck.InternalFluentTQL/src/main/java/de/fraunhofer/iem/secucheck/InternalFluentTQL/dsl/QueriesSet.java
+++ de.fraunhofer.iem.secucheck.InternalFluentTQL/src/main/java/de/fraunhofer/iem/secucheck/InternalFluentTQL/dsl/QueriesSet.java
@@ -36,4 +36,39 @@ public class QueriesSet extends FluentTQLSpecificationImpl {
         taintFlowQueriesAsList.addAll(taintFlowQueries);
         return taintFlowQueriesAsList;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+
+        result = prime * result + taintFlowQueries.hashCode();
+
+        // result = prime * result + ((categoryName == null) ? 0 : categoryName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+
+        QueriesSet other = (QueriesSet) obj;
+        /*if (categoryName == null) {
+            if (other.getCategoryName() != null)
+                return false;
+        } else if (!categoryName.equals(other.getCategoryName()))
+            return false;*/
+
+        if (taintFlowQueries.size() != other.getTaintFlowQueries().size()) return false;
+
+        if (!taintFlowQueries.containsAll(other.getTaintFlowQueries())) return false;
+        return other.getTaintFlowQueries().containsAll(taintFlowQueries);
+    }
+
+    @Override
+    public String toString() {
+        return "QueriesSet(\"" + categoryName + "\")[" + taintFlowQueries.size() + "]";
+    }
 }
