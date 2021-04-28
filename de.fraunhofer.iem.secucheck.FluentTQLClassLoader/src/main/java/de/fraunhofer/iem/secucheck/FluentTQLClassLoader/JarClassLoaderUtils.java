93dab09365a510677b90fd8273b886daf6bebad6
Ranjith K <ranjith.masthikatte@gmail.com>
2021-04-28 19:39:44 +0200

    Checks for the uniqueness of the TaintFlowQuery ID. If there is a duplicate id found then it throws DupliucateTaintFlowQueryIDException

--- de.fraunhofer.iem.secucheck.FluentTQLClassLoader/src/main/java/de/fraunhofer/iem/secucheck/FluentTQLClassLoader/JarClassLoaderUtils.java
+++ de.fraunhofer.iem.secucheck.FluentTQLClassLoader/src/main/java/de/fraunhofer/iem/secucheck/FluentTQLClassLoader/JarClassLoaderUtils.java
@@ -1,6 +1,10 @@
 package de.fraunhofer.iem.secucheck.FluentTQLClassLoader;
 
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.dsl.QueriesSet;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.dsl.exception.DuplicateTaintFlowQueryIDException;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.FluentTQLSpecification;
 import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.MethodPackage.Method;
+import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.Query.TaintFlowQuery;
 import de.fraunhofer.iem.secucheck.InternalFluentTQL.fluentInterface.SpecificationInterface.FluentTQLUserInterface;
 import org.xeustechnologies.jcl.JarClassLoader;
 import org.xeustechnologies.jcl.JclObjectFactory;
@@ -9,6 +13,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 
 /**
  * This utility class provides the feature of loading the FluentTQL related class using the JarClassLoader.
@@ -21,6 +26,7 @@ public class JarClassLoaderUtils {
     private final PrintUtils printUtils = new PrintUtils();
     private final HashSet<String> entryPoints = new HashSet<>();
     private final HashSet<Method> generalPropagators = new HashSet<>();
+    private final HashSet<String> uniqueID = new HashSet<>();
 
     /**
      * This method returns the sets of method signature annotated as AnalysisEntryPoints that is process in the previous call of loadAppAndGetFluentTQLSpecification
@@ -55,8 +61,9 @@ public class JarClassLoaderUtils {
      *
      * @param path FluentTQL specifications Jar path
      * @return HashMap of class name and its corresponding FluentTQLUserInterface
+     * @throws DuplicateTaintFlowQueryIDException If there is duplicate TaintFlowQuery id defined in the given list of specifications
      */
-    public HashMap<String, FluentTQLUserInterface> loadAppAndGetFluentTQLSpecification(String path) {
+    public HashMap<String, FluentTQLUserInterface> loadAppAndGetFluentTQLSpecification(String path) throws DuplicateTaintFlowQueryIDException {
         return loadAppAndGetFluentTQLSpecification(path, false);
     }
 
@@ -66,12 +73,14 @@ public class JarClassLoaderUtils {
      * @param path          FluentTQL specifications Jar path
      * @param isPrettyPrint Pretty Print the Status of each Class
      * @return HashMap of class name and its corresponding FluentTQLUserInterface
+     * @throws DuplicateTaintFlowQueryIDException If there is duplicate TaintFlowQuery id defined in the given list of specifications
      */
-    public HashMap<String, FluentTQLUserInterface> loadAppAndGetFluentTQLSpecification(String path, boolean isPrettyPrint) {
+    public HashMap<String, FluentTQLUserInterface> loadAppAndGetFluentTQLSpecification(String path, boolean isPrettyPrint) throws DuplicateTaintFlowQueryIDException {
         errors.getErrors().clear();
         fluentTQLSpecs.clear();
         entryPoints.clear();
         generalPropagators.clear();
+        uniqueID.clear();
 
         JarClassLoader jarClassLoader = new JarClassLoader();
         jarClassLoader.add(path);
@@ -134,6 +143,8 @@ public class JarClassLoaderUtils {
                                 "There is no FluentTQL Specification available in the given Jar.",
                                 false);
                 }
+            } catch (DuplicateTaintFlowQueryIDException ex) {
+                throw ex;
             } catch (Exception | Error ex) {
                 StringWriter sw = new StringWriter();
                 PrintWriter pw = new PrintWriter(sw);
@@ -156,10 +167,14 @@ public class JarClassLoaderUtils {
      * This method process the FluentTQL related classes
      *
      * @param obj FluentTQL related Object
+     * @throws DuplicateTaintFlowQueryIDException If there is duplicate TaintFlowQuery id defined in the given list of specifications
      */
-    private boolean processFluentTQLObject(Object obj, boolean isPrettyPrint) {
+    private boolean processFluentTQLObject(Object obj, boolean isPrettyPrint) throws DuplicateTaintFlowQueryIDException {
         if (obj instanceof FluentTQLUserInterface) {
             FluentTQLUserInterface fluentTQLUserInterface = (FluentTQLUserInterface) obj;
+
+            checkForUniqueTaintFlowQueryID(fluentTQLUserInterface.getFluentTQLSpecification());
+
             fluentTQLSpecs.put(obj.getClass().getCanonicalName(), fluentTQLUserInterface);
 
             if (isPrettyPrint)
@@ -176,5 +191,47 @@ public class JarClassLoaderUtils {
         }
 
         return fluentTQLSpecs.size() > 0;
+    }
+
+    /**
+     * This method verifies that all the TaintFlowQueries has a unique ID.
+     *
+     * @param specs FluentTQLSpecifications
+     * @throws DuplicateTaintFlowQueryIDException If there is duplicate TaintFlowQuery id defined in the given list of specifications
+     */
+    private void checkForUniqueTaintFlowQueryID(List<FluentTQLSpecification> specs) throws DuplicateTaintFlowQueryIDException {
+        for (FluentTQLSpecification spec : specs) {
+            if (spec instanceof TaintFlowQuery) {
+                checkForUniqueTaintFlowQueryIDInTFQ((TaintFlowQuery) spec);
+            } else if (spec instanceof QueriesSet) {
+                checkForUniqueTaintFlowQueryIDInQS((QueriesSet) spec);
+            }
+        }
+    }
+
+    /**
+     * This method verifies that the given single TaintFlowQuery has a unique ID compare to the last seen TaintFlowQueries.
+     *
+     * @param taintFlowQuery Single TaitFlowQuery
+     * @throws DuplicateTaintFlowQueryIDException If there is duplicate TaintFlowQuery id defined in the given list of specifications
+     */
+    private void checkForUniqueTaintFlowQueryIDInTFQ(TaintFlowQuery taintFlowQuery) throws DuplicateTaintFlowQueryIDException {
+        if (uniqueID.contains(taintFlowQuery.getId())) {
+            throw new DuplicateTaintFlowQueryIDException(taintFlowQuery.getId());
+        } else {
+            uniqueID.add(taintFlowQuery.getId());
+        }
+    }
+
+    /**
+     * This method verifies that the given queriesSet contains all TaintFlowQueries with unique ID compare to the last seen TaintFlowQueries.
+     *
+     * @param queriesSet Queries Set
+     * @throws DuplicateTaintFlowQueryIDException If there is duplicate TaintFlowQuery id defined in the given list of specifications
+     */
+    private void checkForUniqueTaintFlowQueryIDInQS(QueriesSet queriesSet) throws DuplicateTaintFlowQueryIDException {
+        for (TaintFlowQuery taintFlowQuery : queriesSet.getTaintFlowQueries()) {
+            checkForUniqueTaintFlowQueryIDInTFQ(taintFlowQuery);
+        }
     }
 }
