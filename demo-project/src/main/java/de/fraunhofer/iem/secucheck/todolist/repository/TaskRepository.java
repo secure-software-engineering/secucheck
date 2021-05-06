687c104442ffc879664d238b57d4307573c9070a
Ranjith K <ranjith.masthikatte@gmail.com>
2021-05-06 15:04:13 +0200

    Removed catalog projects from the secucheck repository

--- demo-project/src/main/java/de/fraunhofer/iem/secucheck/todolist/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package de.fraunhofer.iem.secucheck.todolist.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import de.fraunhofer.iem.secucheck.todolist.model.Task;
-
-public interface TaskRepository extends CrudRepository<Task, Long> {
-
-}
